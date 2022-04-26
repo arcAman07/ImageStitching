96c7f0af0e060bb3a430b4bcad22a0f243c75603
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-04-27 02:16:57 +0530

    Initial commit

--- src/homography.jl
+++ src/homography.jl
@@ -16,3 +16,29 @@ function Base.show(io::IO, mime::MIME"text/plain", h::DiffImages.Homography{K}) 
     println(io, "DiffImages.Homography{$K} with:")
     show(io, mime, h.H)
 end
+
+function compute_homography(matches::Array{Keypoints})
+    # eigenvector of A^T A with the smallest eigenvalue
+
+    # construct A matrix
+    A = zeros(2 * length(matches), 9)
+    for (index, match) in enumerate(matches)
+        match1, match2 = match
+        base_index_x = index * 2 - 1
+        base_index_y = 1:3
+        A[base_index_x, base_index_y] = float([match1.I...; 1])
+        A[base_index_x+1, 4:6] = A[base_index_x, base_index_y]
+        A[base_index_x, 7:9] =
+            -1.0 * A[base_index_x, base_index_y] * match2.I[1]
+        A[base_index_x+1, 7:9] =
+            -1.0 * A[base_index_x, base_index_y] * match2.I[2]
+    end
+
+    A
+    # find the smallest eigenvector, normalize, and reshape
+    U, S, Vt = svd(A)
+
+    # normalize the homography at the end, since we know the (3, 3)
+    # entry should be 1.
+    Homography(reshape(Vt[:, end] ./ Vt[end][end], (3, 3))')
+end
