6da44f972fd7994c076d8fc3f59ed03b7b6a7afb
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-05-02 22:04:25 +0530

    Initial commit

--- src/funcs.jl
+++ src/funcs.jl
@@ -94,16 +94,3 @@ function fdiff!(dst::AbstractArray, src::AbstractArray;
     return dst
 end
 # Writing the adjoint for the 
-
-@adjoint function fdiff(A::AbstractArray{T,N}; kwargs...) where {T,N}
-    e = eltype(A)
-    y = fdiff!(similar(A, maybe_floattype(eltype(A))), A; kwargs...)
-    final = similar(A,Tuple{eltype(A),eltype(A)})
-    function pullback(Δ)
-        for i in range(1,size(final)[1]), j in range(1,size(final)[2])
-            final[i,j] = (Δ,-Δ)
-        end
-    return final
-    end
-    return (y,pullback)
-end
