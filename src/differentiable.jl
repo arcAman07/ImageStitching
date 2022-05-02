0ce95d225dfef13f2281225f2eef471e2b08d642
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-05-02 22:04:43 +0530

    Initial commit

---
+++ src/differentiable.jl
@@ -0,0 +1,12 @@
+@adjoint function fdiff(A::AbstractArray{T,N}; kwargs...) where {T,N}
+    e = eltype(A)
+    y = fdiff!(similar(A, maybe_floattype(eltype(A))), A; kwargs...)
+    final = similar(A,Tuple{eltype(A),eltype(A)})
+    function pullback(Δ)
+        for i in range(1,size(final)[1]), j in range(1,size(final)[2])
+            final[i,j] = (Δ,-Δ)
+        end
+    return final
+    end
+    return (y,pullback)
+end
