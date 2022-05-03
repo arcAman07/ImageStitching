95ce993690b095143a8a7fd24fb09248afe0b17f
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-05-04 01:35:42 +0530

    sumfinite adjoint added

--- src/differentiable.jl
+++ src/differentiable.jl
@@ -1,11 +1,21 @@
-
+using ImageBase
 # Writing the adjoint for the fdiff function to make it differentiable( AD )
 
 @adjoint function fdiff(A::AbstractArray{T,N}; kwargs...) where {T,N}
-    y = fdiff!(similar(A, maybe_floattype(eltype(A))), A; kwargs...)
+    y = ImageBase.fdiff!(similar(A, maybe_floattype(eltype(A))), A; kwargs...)
     final = similar(A, Tuple{eltype(A),eltype(A)})
     function pullback(Δ)
         fill!(final, (Δ, -Δ))
+        return (final,)
+    end
+    return (y, pullback)
+end
+
+@adjoint function sumfinite(A::AbstractArray{T,N}; kwargs...) where {T,N}
+    y = ImageBase.sumfinite(identity, A; kwargs...)
+    final = similar(A, eltype(A))
+    function pullback(Δ)
+        fill!(final, Δ)
         return (final,)
     end
     return (y, pullback)
