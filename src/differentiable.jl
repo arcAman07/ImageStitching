3d30bb702b52177360006a8c5d99b4d2865e66e1
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-05-03 20:18:04 +0530

    Initial commit

--- src/differentiable.jl
+++ src/differentiable.jl
@@ -2,7 +2,6 @@
 # Writing the adjoint for the fdiff function to make it differentiable( AD )
 
 @adjoint function fdiff(A::AbstractArray{T,N}; kwargs...) where {T,N}
-    e = eltype(A)
     y = fdiff!(similar(A, maybe_floattype(eltype(A))), A; kwargs...)
     final = similar(A, Tuple{eltype(A),eltype(A)})
     function pullback(Δ)
