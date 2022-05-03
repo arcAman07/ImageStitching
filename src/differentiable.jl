7c972d479d519cbd149d9221bbcf3e8b4f4d4327
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-05-03 16:26:04 +0530

    Updated adjoint

--- src/differentiable.jl
+++ src/differentiable.jl
@@ -4,12 +4,10 @@
 @adjoint function fdiff(A::AbstractArray{T,N}; kwargs...) where {T,N}
     e = eltype(A)
     y = fdiff!(similar(A, maybe_floattype(eltype(A))), A; kwargs...)
-    final = similar(A,Tuple{eltype(A),eltype(A)})
+    final = similar(A, Tuple{eltype(A),eltype(A)})
     function pullback(Δ)
-        for i in range(1,size(final)[1]), j in range(1,size(final)[2])
-            final[i,j] = (Δ,-Δ)
-        end
-    return final
+        fill!(final, (Δ, -Δ))
+        return (final,)
     end
-    return (y,pullback)
+    return (y, pullback)
 end
